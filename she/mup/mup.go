d11ea21d647c61cd06b0dd488693926e8820017a
Andrei Burdulescu <andrei.burdulescu@pm.me>
2023-12-18 20:48:11 +0200

    Rename mup pkg and move KeyID one level up

--- she/mup/mup.go
+++ she/mup/mup.go
@@ -1,8 +1,10 @@
-package mupfmt
+package mup
 
 import (
 	"encoding/hex"
 	"fmt"
+
+	"github.com/aburdulescu/autosar/she"
 )
 
 type ProtectionFlags struct {
@@ -23,13 +25,13 @@ type Input struct {
 	UID string
 
 	// ID of the secret key
-	AuthID KeyID
+	AuthID she.KeyID
 
 	// secret key as hex string
 	AuthKey string
 
 	// ID of the slot to be updated
-	ID KeyID
+	ID she.KeyID
 
 	// the new value of the key as hex string
 	NewKey string
@@ -41,12 +43,14 @@ type Input struct {
 	Flags ProtectionFlags
 }
 
+// memory update protocol encode result
 type EncodeResult struct {
 	M1 [16]byte
 	M2 [32]byte
 	M3 [16]byte
 }
 
+// Encode the memory update protocol input
 func (in Input) Encode() (*EncodeResult, error) {
 	uid, err := hex.DecodeString(in.UID)
 	if err != nil {
@@ -78,87 +82,4 @@ func (in Input) Encode() (*EncodeResult, error) {
 
 	var result EncodeResult
 	return &result, nil
-}
-
-//go:generate stringer -type=KeyID
-type KeyID uint8
-
-const (
-	SECRET_KEY KeyID = iota
-	MASTER_ECU_KEY
-	BOOT_MAC_KEY
-	BOOT_MAC
-	KEY_1
-	KEY_2
-	KEY_3
-	KEY_4
-	KEY_5
-	KEY_6
-	KEY_7
-	KEY_8
-	KEY_9
-	KEY_10
-	RAM_KEY
-)
-
-func (id KeyID) IsValid() bool {
-	return id < KeyID(len(_KeyID_index)-1)
-}
-
-func (id KeyID) IsCompatible(other KeyID) error {
-	if !id.IsValid() {
-		return fmt.Errorf("%s is not valid", id)
-	}
-	if !other.IsValid() {
-		return fmt.Errorf("%s is not valid", other)
-	}
-
-	switch id {
-	case
-		MASTER_ECU_KEY,
-		BOOT_MAC_KEY,
-		BOOT_MAC:
-
-		if other != id {
-			return fmt.Errorf("%s can be updated only with itself", id)
-		}
-
-	case
-		KEY_1,
-		KEY_2,
-		KEY_3,
-		KEY_4,
-		KEY_5,
-		KEY_6,
-		KEY_7,
-		KEY_8,
-		KEY_9,
-		KEY_10:
-
-		if other != id && other != MASTER_ECU_KEY {
-			return fmt.Errorf("%s can be updated only with itself or %s", id, MASTER_ECU_KEY)
-		}
-
-	case RAM_KEY:
-
-		if other != SECRET_KEY &&
-			other != KEY_1 &&
-			other != KEY_2 &&
-			other != KEY_3 &&
-			other != KEY_4 &&
-			other != KEY_5 &&
-			other != KEY_6 &&
-			other != KEY_7 &&
-			other != KEY_8 &&
-			other != KEY_9 &&
-			other != KEY_10 {
-			return fmt.Errorf("%s can be updated only with %s or one of KEY_n", id, SECRET_KEY)
-		}
-
-	default:
-		return fmt.Errorf("%s is not valid or cannot be used", id)
-
-	}
-
-	return nil
 }
--- she/mup/mup_test.go
+++ she/mup/mup_test.go
@@ -1,15 +1,17 @@
-package mupfmt
+package mup
 
 import (
 	"encoding/hex"
 	"testing"
+
+	"github.com/aburdulescu/autosar/she"
 )
 
-func TestEncode(t *testing.T) {
+func TestMUPEncode(t *testing.T) {
 	in := Input{
 		UID:     "000000000000000000000000000001",
-		AuthID:  MASTER_ECU_KEY,
-		ID:      KEY_1,
+		AuthID:  she.MASTER_ECU_KEY,
+		ID:      she.KEY_1,
 		AuthKey: "000102030405060708090a0b0c0d0e0f",
 		NewKey:  "0f0e0d0c0b0a09080706050403020100",
 		Counter: 1,
