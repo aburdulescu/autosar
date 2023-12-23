e47765f3da6f7fc55ec7d6658cdcb8a8a735c030
Andrei Burdulescu <andrei.burdulescu@pm.me>
2023-12-23 08:46:56 +0200

    internal/crypto/aes/cmac: Finish Generate

--- internal/crypto/aes/cmac/cmac.go
+++ internal/crypto/aes/cmac/cmac.go
@@ -2,39 +2,36 @@ package cmac
 
 import (
 	"crypto/aes"
-	"errors"
+	"crypto/cipher"
 )
 
 const bs = 16
 
-func Sum(key, msg []byte) ([]byte, error) {
+func Generate(key, msg []byte) ([]byte, error) {
 	c, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
 	}
 
-	if len(msg)%bs != 0 {
-		return nil, errors.New("unaligned input not supported for now")
-	}
-
 	aligned := len(msg)%bs == 0
 
-	n := len(msg) / bs
+	n := (len(msg) + 15) / bs
 
 	if n == 0 {
 		n = 1
 		aligned = false
 	}
 
-	var k1 [bs]byte
-	var k2 [bs]byte
+	var k1, k2 [bs]byte
+
+	generateSubKey(c, k1[:], k2[:])
 
 	var mLast [bs]byte
 	if aligned {
-		copy(mLast[:], msg[(len(msg)-1)*16:])
+		copy(mLast[:], msg[(n-1)*16:])
 		xor(mLast[:], k1[:])
 	} else {
-		copy(mLast[:], msg[(len(msg)-1)*16:])
+		pad(mLast[:], msg[(n-1)*16:])
 		xor(mLast[:], k2[:])
 	}
 
@@ -52,8 +49,45 @@ func Sum(key, msg []byte) ([]byte, error) {
 	return mLast[:], nil
 }
 
+func generateSubKey(c cipher.Block, k1, k2 []byte) {
+	var constRb = [bs]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x87}
+
+	var l [bs]byte
+
+	c.Encrypt(l[:], l[:])
+
+	shift(k1, l[:])
+
+	if l[0]&0x80 != 0 {
+		xor(k1, constRb[:])
+	}
+
+	shift(k2, k1)
+
+	if k1[0]&0x80 != 0 {
+		xor(k2, constRb[:])
+	}
+
+}
+
+func shift(dst, src []byte) {
+	overflow := false
+	for i := len(src) - 1; i >= 0; i-- {
+		dst[i] = src[i] << 1
+		if overflow {
+			dst[i] |= 1
+		}
+		overflow = src[i]&0x80 != 0
+	}
+}
+
+func pad(dst, src []byte) {
+	copy(dst, src)
+	dst[len(src)] = 0x80
+}
+
 func xor(dst, src []byte) {
-	for i := range dst {
-		dst[i] = dst[i] ^ src[i]
+	for i, v := range src {
+		dst[i] ^= v
 	}
 }
